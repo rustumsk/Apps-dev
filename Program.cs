52f1723c34ad8cff66490f62a5310b6fe2a08cd3
russ <136210947+rustumsk@users.noreply.github.com>
2024-02-21 19:52:54 +0800

    Updated the validation of inputs, nothing can break this nigga anymore

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using ConsoleApp1;
+using ConsoleApp1;
 using System;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,47 +20,70 @@ namespace ConsoleApp1
             Console.Write("\nEnter Vehicle Brand: ");
             string vehBrand = Console.ReadLine();
 
-            Console.Write("\nVehicle type: 1. MotorBike 2.SUV/VAN 3.Sedan: ");
-            int vehOpt = Convert.ToInt32(Console.ReadLine());
-
             string vehType = "";
-
-            Suv suv = new Suv(" ", " ", " ");
-            MotorBike motor = new MotorBike(" ", " ", " ");
-            Sedan sedan = new Sedan(" ", " ", " ");
+            int outerSwitchLoop = 0;
             int initial = 0;
             int proceeding = 0;
-            switch (vehOpt)
-            {
-                case 1:
-                    vehType = "MotorBike";
-                    motor = new MotorBike(vehBrand, plateNumber, "MotorCycle");
-                    plateNumber = motor.PlateNum;
-                    vehBrand = motor.Brand;
-                    vehType = motor.Type;
-                    initial = (int)MotorBike.Amounts.value;
-                    proceeding = (int)MotorBike.Amounts.perHour;
-                    break;
-                case 2:
-                    vehType = "SUV/VAN";
-                    suv = new Suv(vehBrand, plateNumber, "Suv");
-                    plateNumber = suv.PlateNum;
-                    vehBrand = suv.Brand;
-                    vehType = suv.Type;
-                    initial = (int)Suv.Amounts.value;
-                    proceeding = (int)Suv.Amounts.perHour;
-                    break;
-                case 3:
-                    vehType = "Sedan";
-                    sedan = new Sedan(vehBrand, plateNumber, "Sedan");
-                    plateNumber = sedan.PlateNum;
-                    vehBrand = sedan.Brand;
-                    vehType = sedan.Type;
-                    initial = (int)Sedan.Amounts.value;
-                    proceeding = (int)Sedan.Amounts.perHour;
-                    break;
-            }
 
+            while (outerSwitchLoop < 1)
+            {
+                int vehOpt = 0;
+
+                int innerSwitchLoop = 0;
+                while (innerSwitchLoop < 1)
+                {
+                    try
+                    {
+                        Console.Write("\nVehicle type: 1. MotorBike 2.SUV/VAN 3.Sedan: ");
+                        vehOpt = Convert.ToInt32(Console.ReadLine());
+                        innerSwitchLoop = 1;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Invalid Input! Integer input only!");
+                    }
+                }
+
+                Suv suv = new Suv(" ", " ", " ");
+                MotorBike motor = new MotorBike(" ", " ", " ");
+                Sedan sedan = new Sedan(" ", " ", " ");
+                switch (vehOpt)
+                {
+                    case 1:
+                        vehType = "MotorBike";
+                        motor = new MotorBike(vehBrand, plateNumber, "MotorCycle");
+                        plateNumber = motor.PlateNum;
+                        vehBrand = motor.Brand;
+                        vehType = motor.Type;
+                        initial = (int)MotorBike.Amounts.value;
+                        proceeding = (int)MotorBike.Amounts.perHour;
+                        outerSwitchLoop = 1;
+                        break;
+                    case 2:
+                        vehType = "SUV/VAN";
+                        suv = new Suv(vehBrand, plateNumber, "Suv");
+                        plateNumber = suv.PlateNum;
+                        vehBrand = suv.Brand;
+                        vehType = suv.Type;
+                        initial = (int)Suv.Amounts.value;
+                        proceeding = (int)Suv.Amounts.perHour;
+                        outerSwitchLoop = 1;
+                        break;
+                    case 3:
+                        vehType = "Sedan";
+                        sedan = new Sedan(vehBrand, plateNumber, "Sedan");
+                        plateNumber = sedan.PlateNum;
+                        vehBrand = sedan.Brand;
+                        vehType = sedan.Type;
+                        initial = (int)Sedan.Amounts.value;
+                        proceeding = (int)Sedan.Amounts.perHour;
+                        outerSwitchLoop = 1;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option!");
+                        break;
+                }
+            }
             Parking park = new Parking(plateNumber, vehType, vehBrand, parkIn);
 
             Console.WriteLine("\n=======Here is your parking detail========");
@@ -105,7 +128,10 @@ namespace ConsoleApp1
                         park.ShowParkingInfo();
                         innerWhileLoop = 0;
                         break;
-
+                    default:
+                        Console.WriteLine("Invalid Option, Please choose a valid option!");
+                        innerWhileLoop = 0;
+                        break;
                 }
             }
             Console.ReadKey();
